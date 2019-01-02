146808d8f21b2a1f10ca49adb8694c0bb990edc4
bredele <olivier.wietrich@gmail.com>
2019-01-02 11:55:42 -0700

    Add documentation

--- index.js
+++ index.js
@@ -1,15 +1,68 @@
+/**
+ * Create event emitter or mixin given object with event
+ * emitter.
+ *
+ * @param {Object?} obj
+ * @return {Object} obj
+ * @api public
+ */
+
 module.exports = function (obj) {
   obj = obj || {}
+
+  /**
+   * Listeners store.
+   * @type {Object}
+   */
+
   var callbacks = {}
+
+  /**
+   * Emit event with a given topic.
+   *
+   * @param {String} topic
+   * @param {Array} values
+   * @api private
+   */
+
   var emit = function (topic, values) {
     (callbacks[topic] || []).map(function (cb) {
       cb.apply(null, values)
     })
   }
+
+  /**
+   * Listen event and execute given callback.
+   *
+   * Examples:
+   *
+   *  emitter.on('hello', cb)
+   *
+   * @param {String} topic
+   * @param {Function} cb
+   * @api public
+   */
+
   obj.on = function (topic, cb) {
     (callbacks[topic] = callbacks[topic] || []).push(cb)
     return obj
   }
+
+  /**
+   * Remove all event listeners, specific listener or
+   * all listeners for a given event.
+   *
+   * Examples:
+   *
+   *  emitter.off('hello')
+   *  emitter.off('hello', fn)
+   *  emitter.off()
+   *
+   * @param {String} topic
+   * @param {Function} cb
+   * @api public
+   */
+
   obj.off = function (topic, cb) {
     var events = callbacks[topic]
     if (!topic) callbacks = {}
@@ -17,6 +70,19 @@ module.exports = function (obj) {
     else callbacks[topic] = []
     return obj
   }
+
+  /**
+   * Listen event once and execute given callback.
+   *
+   * Examples:
+   *
+   *  emitter.once('hello', cb)
+   *
+   * @param {String} topic
+   * @param {Function} cb
+   * @api public
+   */
+
   obj.once = function (topic, cb) {
     var fn = function () {
       cb.apply(null, arguments)
@@ -25,11 +91,27 @@ module.exports = function (obj) {
     obj.on(topic, fn)
     return obj
   }
+
+  /**
+   * Emit event
+   *
+   * Examples:
+   *
+   *  emitter.emit('hello')
+   *  emitter.emit('hello', 'world')
+   *  emitter.emit('hello', 'world', 'universe')
+   *
+   * @param {String} topic
+   * @param {Function} cb
+   * @api public
+   */
+
   obj.emit = function () {
     var args = [].slice.call(arguments)
     emit(args.shift(), args)
     emit('*', args)
     return obj
   }
+
   return obj
 }
