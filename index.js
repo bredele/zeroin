b94dd66adda4b7ef93d382c7c9fcfdd19ad4e845
bredele <olivier.wietrich@gmail.com>
2019-01-02 11:00:59 -0700

    Listen event

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 module.exports = function () {
+  var callbacks = {}
   return {
-    on: function () {},
+    on: function (topic, cb) {
+      (callbacks[topic] = callbacks[topic] || []).push(cb)
+    },
     once: function () {},
     off: function () {},
-    emit: function () {}
+    emit: function (topic) {
+      callbacks[topic].map(function (cb) {
+        cb()
+      })
+    }
   }
 }
--- test/zeroin.js
+++ test/zeroin.js
@@ -13,3 +13,18 @@ test('should have the following API', assert => {
   assert.equal(typeof emitter.once, 'function')
   assert.equal(typeof emitter.off, 'function')
 })
+
+test('should emit and listen event', assert => {
+  assert.plan(1)
+  const emitter = pubsub()
+  emitter.on('hello', () => assert.ok('pass'))
+  emitter.emit('hello')
+})
+
+test('should emit and listen event multiple times', assert => {
+  assert.plan(2)
+  const emitter = pubsub()
+  emitter.on('hello', () => assert.ok('pass'))
+  emitter.on('hello', () => assert.ok('pass'))
+  emitter.emit('hello')
+})
